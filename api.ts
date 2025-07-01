78e0e2e9917cb69c7fdedc075852820e42b8c253
Derck23 <eramireznieves25@gmail.com>
2025-06-30 18:28:06 -0600

    cors

--- api.ts
+++ api.ts
@@ -1,8 +1,14 @@
 import { Application } from "@oak/oak";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { ruta } from "./routes/authRoutes.ts";
 
 const app = new Application();
 const port = 8080;
+
+app.use(oakCors({
+  origin: "http://localhost:5173", // Cambia esto si tu frontend está en otro puerto/origen
+  credentials: true,
+}));
 
 app.use(ruta.routes());
 app.use(ruta.allowedMethods());
