da0afb6b4729577ccdd465b48fea4403459190ba
Jose Manuel Martinez Martinez <jmanuelm94@outlook.com>
2025-07-24 17:16:31 -0600

    se corrigio el horario

--- utils/generadorHorario.ts
+++ utils/generadorHorario.ts
@@ -5,7 +5,7 @@ import { Horario } from '../interfaces/i_horario.ts';
 
 const DIAS = ["lunes", "martes", "miércoles", "jueves", "viernes"];
 const HORAS = ["7:20", "8:10", "9:00", "9:50", "10:00", "10:50", "11:40", "12:05", "12:55", "13:45"];
-const HORA_RECESO = ["9:50", "12:05"];
+const HORA_RECESO = ["9:50", "11:40"];
 
 export function generarHorario(
     grupo: Grupo,
